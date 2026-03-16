6b80f311a2d8c2b8bdcc9e2729b04dbb4f153f54
marco <marcomorenilla@gmail.com>
2026-03-16 18:10:07 +0100

    implemento drag and drop

--- js/state.js
+++ js/state.js
@@ -1,11 +1,12 @@
-
 export const state = {
-    isLoading: true,
-    favorites: JSON.parse(localStorage.getItem('favorites')) || [],
-    fullPokemonList: new Map(),
-    filteredPokemonList: new Map(),
-    pagination: {
-        init: 151,
-        offset: 12
-    }
-}
+  isLoading: true,
+  favorites: JSON.parse(localStorage.getItem("favorites")) || [],
+  fullPokemonList: new Map(),
+  filteredPokemonList: new Map(),
+  pagination: {
+    init: 151,
+    offset: 12,
+  },
+  filters: new Set(),
+  team1: JSON.parse(localStorage.getItem("team1")) || [],
+};
